5506fb0c78148220266e0df930c82221523c4f44
Onyedikachi Geoffrey <jecintageoffrey@yahoo.com>
2025-12-01 16:10:58 +0100

    Integrate Application, Infrastructure, and Persistence Layers into DI Container

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            // Add application services here
+        }
+    }
+}
---
+++ Persistance/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistance
+{
+    public static class ServiceExtensions
+    {
+        public static void AddPersistance(this IServiceCollection services)
+        {
+            // Add persistence services here
+        }
+    }
+}
