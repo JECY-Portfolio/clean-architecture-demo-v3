101eea0066a5b29ea9211ed6afb5e65aff57bb77
Onyedikachi Geoffrey <jecintageoffrey@yahoo.com>
2025-12-02 17:26:25 +0100

    Implement Fluent Validation using MediatR Pipeline Behaviors for Request Validation

---
+++ Application/Behaviours/ValidationBehaviours.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Application.Behaviours
+{
+    public class ValidationBehaviours<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validator;
+        public ValidationBehaviours(IEnumerable<IValidator<TRequest>> validator)
+        {
+            _validator = validator;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            //Pre-Processing logic here
+            //For Example, Logging, validation
+
+            if (_validator.Any())
+            {
+                var validationContext = new ValidationContext<TRequest>(request);
+                var result = await Task.WhenAll(_validator.Select(v => v.ValidateAsync(validationContext, cancellationToken)));
+                var failers = result.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failers.Count > 0)
+                {
+                    throw new ValidationException(failers);
+                }
+            }
+
+            //Next
+
+            var response = await next();
+
+            //Post-Processing logic here
+            //For Example, Response modification...
+
+            return response;
+        }
+    }
+}
--- Application/ServiceExtensions.cs
+++ Application/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Application
@@ -10,6 +13,10 @@ namespace Application
             // Add application services here
             services.AddAutoMapper(Assembly.GetExecutingAssembly());    
             services.AddMediatR(conf => conf.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            //register validators
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviours<,>));
         }
     }
 }
