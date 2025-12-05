d4917b5a6066212fab9f6ad5b9fe6b619079c6b6
Onyedikachi Geoffrey <jecintageoffrey@yahoo.com>
2025-12-05 11:24:40 +0100

    Implement Resend Email Confirmation API and Logic

--- Application/Interfaces/IAccountService.cs
+++ Application/Interfaces/IAccountService.cs
@@ -8,6 +8,6 @@ namespace Application.Interfaces
         Task<ApiResponse<AuthenticationResponse>> Authenticate(AuthenticationRequest request);
         Task<ApiResponse<Guid>> RegisterUser(RegisterRequest registerRequest);
         Task<ApiResponse<bool>> ConfirmEmail(string userId, string token);
-
+        Task<ApiResponse<bool>> ResendConfirmationEmailAsync(string email);
     }
 }
